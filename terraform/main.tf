220e0de678f754c21d1ac791e87e196991044094
M. Saad Naeem <100247988+Saad-naeem0@users.noreply.github.com>
2025-06-30 07:05:14 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,6 +51,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
   size                = "Standard_B1s"
   admin_username      = "azureuser"
   disable_password_authentication = true
+  custom_data = filebase64("${path.module}/cloud-init.yml")
+
 
   admin_ssh_key {
     username   = "azureuser"
